9bfac4b2cbfb52fd44b82c35c88c77f2d4b057ac
Eltayeb Ahmed <eltayeb1000@yahoo.com>
2017-05-29 12:54:40 +0300

    redo again#

--- docs/builld_docs.py
+++ docs/builld_docs.py
@@ -3,8 +3,9 @@ from os import path
 from os import getcwd
 pdf_reader_path = r"C:\Program Files (x86)\Mozilla Firefox\firefox.exe"
 
-chart = "logic_flow_chart"
-builder = Popen(["dot", ("%s.dot" % chart), "-O", "-Tpdf"])
-builder.communicate()
-pdf_reader = Popen([pdf_reader_path, path.join(getcwd(), "%s.dot.pdf" % chart)])
-pdf_reader.communicate()
+charts = ["logic_flow_chart", "class_flow_chart"]
+for chart in charts:
+    builder = Popen(["dot", ("%s.dot" % chart), "-O", "-Tpdf"])
+    builder.communicate()
+    pdf_reader = Popen([pdf_reader_path, path.join(getcwd(), "%s.dot.pdf" % chart)])
+    pdf_reader.communicate()
